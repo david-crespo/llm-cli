acbef257a850a1ec10740b8764fe339d9e7aab89
David Crespo <david.crespo@oxidecomputer.com>
2024-03-16 02:23:36 -0500

    works with claude. that rules

--- chat-gpt.ts
+++ chat-gpt.ts
@@ -3,32 +3,42 @@ import * as flags from "https://deno.land/std@0.184.0/flags/mod.ts"
 import { readAll } from "https://deno.land/std@0.184.0/streams/read_all.ts"
 import { type JSONValue } from "https://deno.land/std@0.184.0/jsonc/mod.ts"
 import OpenAI from "https://deno.land/x/openai@v4.16.1/mod.ts"
+import "https://deno.land/std@0.219.0/dotenv/load.ts"
+import Anthropic from "npm:@anthropic-ai/sdk@0.18.0"
 
 // considered annotating each response with the model that generated it, but
 // let's first see if requiring them all to be the same model is tolerable
 
+type AssistantMessage = { role: "assistant"; model: string; content: string }
+type UserMessage = { role: "user"; content: string }
+type ChatMessage = UserMessage | AssistantMessage
+
 type Chat = {
-  // For now we don't allow model or system prompt to be changed in the middle
-  // of a chat. Otherwise we'd have to annotate each message with both.
-  model: string
-  systemPrompt: string | null
-  messages: {
-    role: "user" | "assistant"
-    content: string
-  }[]
+  // For now we don't allow system prompt to be changed in the middle
+  // of a chat. Otherwise we'd have to annotate each message with it.
+  systemPrompt: string | undefined
+  messages: ChatMessage[]
   // making this a string means we don't have to think about parsing
   createdAt: string
 }
 
 // send message to the API and return the response
-type CreateMessage = (chat: Chat, input: string, turbo: boolean) => Promise<string>
+type CreateMessage = (
+  chat: Chat,
+  input: string,
+  turbo: boolean,
+) => Promise<AssistantMessage>
+
+const msgsForApi = (
+  messages: ChatMessage[],
+  input: string,
+) => [
+  // need to take the model key out of AssistantMessage or OpenAI gets mad
+  ...messages.map((m) => ({ role: m.role, content: m.content })),
+  { role: "user" as const, content: input },
+]
 
 // --------------------------------
-
-type GptMessage = {
-  role: "user" | "assistant" | "system"
-  content: string
-}
 
 const gptCreateMessage: CreateMessage = async function (chat, input, turbo) {
   const openai = new OpenAI()
@@ -36,20 +46,32 @@ const gptCreateMessage: CreateMessage = async function (chat, input, turbo) {
   const systemMsg = chat.systemPrompt
     ? [{ role: "system" as const, content: chat.systemPrompt }]
     : []
-  const messages: GptMessage[] = [
-    ...systemMsg,
-    ...chat.messages,
-    { role: "user", content: input },
-  ]
-  if (chat.systemPrompt) {
-    messages.unshift({ role: "system" as const, content: chat.systemPrompt })
-  }
-
+  const messages = [...systemMsg, ...msgsForApi(chat.messages, input)]
   const resp = await openai.chat.completions.create({ model, messages })
-  const respStr = resp.choices[0].message?.content
-  if (!respStr) throw new Error("No response found")
-  return respStr
+  const content = resp.choices[0].message?.content
+  if (!content) throw new Error("No response found")
+  return { role: "assistant", model, content }
 }
+
+// --------------------------------
+
+const claudeCreateMessage: CreateMessage = async function (chat, input, turbo) {
+  const anthropic = new Anthropic({
+    apiKey: Deno.env.get("ANTHROPIC_API_KEY"),
+  })
+
+  const model = turbo ? "claude-3-sonnet-20240229" : "claude-3-opus-20240229"
+  const response = await anthropic.messages.create({
+    max_tokens: 1024,
+    system: chat.systemPrompt,
+    messages: msgsForApi(chat.messages, input),
+    model,
+  })
+  const content = response.content[0].text
+  return { role: "assistant", model, content }
+}
+
+// --------------------------------
 
 // SETUP: put OPENAI_API_KEY in a .env file in the same directory as this script
 
@@ -71,11 +93,12 @@ Pass \`-\` as message to read from stdin.
 | Flag | Effect |
 | --- | --- |
 | None | Start a new chat |
+| -c, --claude | Use Claude instead of GPT |
 | -r, --reply | Continue existing chat |
 | -s, --show | Show chat so far |
 | -a, --append | Read from stdin and append to MESSAGE |
 | -p, --persona [str] | Override default persona in system prompt |
-| -t, --turbo | Use gpt-3.5-turbo instead of gpt-4 |
+| -t, --turbo | Use a faster, cheaper model |
 `
 
 const History = {
@@ -89,14 +112,32 @@ const History = {
   },
 }
 
+function printChat(chat: Chat) {
+  console.log(`**Chat started:** ${chat.createdAt}\n`)
+  console.log(`**System prompt:** ${chat.systemPrompt}`)
+  for (const msg of chat.messages) { // skip system prompt
+    const model = msg.role === "assistant" ? ` (${msg.model})` : ""
+    console.log(`# ${msg.role}${model}\n\n`)
+    console.log(`${msg.content}\n`)
+  }
+}
+
 const getStdin = async () => new TextDecoder().decode(await readAll(Deno.stdin)).trim()
 
 // === script starts here ===
 
 const args = flags.parse(Deno.args, {
-  boolean: ["help", "reply", "show", "append", "turbo"],
+  boolean: ["help", "claude", "reply", "show", "append", "turbo"],
   string: ["persona"],
-  alias: { h: "help", r: "reply", s: "show", a: "append", p: "persona", t: "turbo" },
+  alias: {
+    h: "help",
+    c: "claude",
+    r: "reply",
+    s: "show",
+    a: "append",
+    p: "persona",
+    t: "turbo",
+  },
 })
 
 if (args.help) {
@@ -108,12 +149,7 @@ const history = History.read()
 
 if (args.show) {
   if (history) {
-    console.log(`**Model:** ${history.model}\n`)
-    console.log(`**Chat started:** ${history.createdAt}\n`)
-    console.log(`**System prompt:** ${history.systemPrompt}`)
-    for (const msg of history.messages) { // skip system prompt
-      console.log(`# ${msg.role}\n\n${msg.content}\n`)
-    }
+    printChat(history)
   } else {
     console.log("No history found")
   }
@@ -136,7 +172,6 @@ const systemPrompt =
 // if there is no history, ignore reply flag
 const chat: Chat = args.reply && history ? history : {
   createdAt: new Date().toLocaleString(),
-  model: args.turbo ? "gpt-3.5-turbo-1106" : "gpt-4-turbo-preview",
   systemPrompt,
   messages: [],
 }
@@ -146,13 +181,16 @@ const input = args.append
   ? directInput + "\n\n" + (await getStdin())
   : (directInput === "-" ? await getStdin() : directInput.toString())
 
+const createMessage = args.claude ? claudeCreateMessage : gptCreateMessage
+
 try {
-  const respMsg = await gptCreateMessage(chat, input, args.turbo)
+  const assistantMsg = await createMessage(chat, input, args.turbo)
   chat.messages.push({ role: "user", content: input })
-  chat.messages.push({ role: "assistant", content: respMsg })
+  chat.messages.push(assistantMsg)
   History.write(chat)
-  console.log(respMsg)
+  console.log(assistantMsg.content)
 } catch (e) {
+  // TODO: update error handling for claude?
   if (e.response?.status) console.log("Request error:", e.response.status)
   if (e.response?.data) console.log(jsonBlock(e.response.data))
   if (!("response" in e)) console.log(e)
